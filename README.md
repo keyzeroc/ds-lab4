fda41d752f4ea95b32123b32a81584ec28f019c1
keyzeroc <gozveruga@gmail.com>
2024-12-08 13:54:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## DS Lab4 - JS - Web scrape Rozetka
+
+To run this code locally, you need to install [Node.js](https://nodejs.org/en/download/) and [NPM](https://www.npmjs.com/get-npm).
+
+Then copy the code from this repository to your local machine by running `git clone https://github.com/keyzeroc/ds-lab4.git`.
+
+After that, run `npm install` to install all dependencies.
+
+To run the code, run `node index.js`.
---
+++ index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// Інтервал перевірки (в мілісекундах)
+const INTERVAL = 60000;
+
+// URL-адреса товару, який наразі недоступний
+const PRODUCT_URL = 'https://rozetka.com.ua/ua/1stcharger-bn1st6027/p437111474/';
+
+// URL-адреса товару, який доступний (для тестування)
+// const PRODUCT_URL = 'https://rozetka.com.ua/ua/lenovo-82vg00rgra/p450313244/';
+
+const fetchAndParse = async () => {
+  try {
+    const { data } = await axios.get(PRODUCT_URL);
+    const $ = cheerio.load(data);
+
+    const status = $('.status-label').text()
+
+    console.log(status)
+  } catch (error) {
+    console.error('Помилка при отриманні даних:', error);
+  }
+};
+// Початкова перевірка
+fetchAndParse();
+
+// Періодична перевірка кожні X секунд
+setInterval(fetchAndParse, INTERVAL);
+
+
